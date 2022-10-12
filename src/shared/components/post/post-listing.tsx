f302b4af027fa5bf5d60083cec30702304156eca
tk77 <tommyknight@protonmail.com>
2022-10-12 18:22:41 -0400

    fix

--- src/shared/components/post/post-listing.tsx
+++ src/shared/components/post/post-listing.tsx
@@ -228,83 +228,6 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
   //   }
   //================= END MY OLD MAIN LISTING can delete
 
-  // MY listing 2 - module - qz3 =========================================================
-  listing() {
-    let post = this.props.post_view.post;
-
-    // IMAGE OR IFRAME POST
-    return post.thumbnail_url || isImage(post.url) ? (
-      <div class="d-block">
-        <div class="row">
-          {!this.props.viewOnly && this.voteBar()}
-          {mobile ? (
-            <div className="col-12 disco-wide tiesto">
-              {this.postTitleLine()}
-              {/*  THE BIG IMAGE moved here */}
-              {this.img}
-              {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
-              {this.showMobilePreview()}
-              {/* post by   ================ */}
-              {this.createdLine()}
-              {this.commentsLine(true)}
-              {/*  {this.commentsLine()}   ================ */}
-              {this.userActionsLine()}
-              {this.duplicatesLine()}
-              {this.removeAndBanDialogs()}
-            </div>
-          ) : (
-            <>
-              <div
-                className={`${
-                  this.state.imageExpanded
-                    ? "col-12 disco-wide"
-                    : "col-8 disco-thin"
-                }`}
-              >
-                {this.postTitleLine()}
-                {/*  THE BIG IMAGE moved here */}
-                {this.state.imageExpanded && this.img}
-                {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
-                {this.showMobilePreview()}
-                {/* post by   ================ */}
-                {this.createdLine()}
-                {this.commentsLine(true)}
-                {/*  {this.commentsLine()}   ================ */}
-                {this.userActionsLine()}
-                {this.duplicatesLine()}
-                {this.removeAndBanDialogs()}
-              </div>
-              <div class="col-4" id="expando-box">
-                {/* Post body prev or thumbnail */}
-                {!this.state.imageExpanded && this.thumbnail()}
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    ) : (
-      <div class="d-block">
-        <div class="row">
-          {/* TEXT POST   ================ */}
-          {!this.props.viewOnly && this.voteBar()}
-          <div className="col-12" id="text-post">
-            {this.postTitleLine()}
-            {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
-            {this.showMobilePreview()}
-            {/* post by   ================ */}
-            {this.createdLine()}
-            {this.commentsLine(true)}
-            {/* {this.commentsLine()}*/}
-            {this.userActionsLine()}
-            {this.duplicatesLine()}
-            {this.removeAndBanDialogs()}
-          </div>
-        </div>
-      </div>
-    );
-  }
-  // ======== END MY LISTING
-
   // // THEIR LISTING - MAIN, unneeded
   // listing() {
   //   return (
@@ -816,6 +739,83 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
       </ul>
     );
   }
+
+  // MY listing 2 - module - qz3 =========================================================
+  listing() {
+    let post = this.props.post_view.post;
+
+    // IMAGE OR IFRAME POST
+    return post.thumbnail_url || isImage(post.url) ? (
+      <div class="d-block">
+        <div class="row">
+          {!this.props.viewOnly && this.voteBar()}
+          {mobile ? (
+            <div className="col-12 disco-wide tiesto">
+              {this.postTitleLine()}
+              {/*  THE BIG IMAGE moved here */}
+              {this.img}
+              {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
+              {this.showMobilePreview()}
+              {/* post by   ================ */}
+              {this.createdLine()}
+              {this.commentsLine(true)}
+              {/*  {this.commentsLine()}   ================ */}
+              {this.userActionsLine()}
+              {this.duplicatesLine()}
+              {this.removeAndBanDialogs()}
+            </div>
+          ) : (
+            <>
+              <div
+                className={`${
+                  this.state.imageExpanded
+                    ? "col-12 disco-wide"
+                    : "col-8 disco-thin"
+                }`}
+              >
+                {this.postTitleLine()}
+                {/*  THE BIG IMAGE moved here */}
+                {this.state.imageExpanded && this.img}
+                {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
+                {this.showMobilePreview()}
+                {/* post by   ================ */}
+                {this.createdLine()}
+                {this.commentsLine(true)}
+                {/*  {this.commentsLine()}   ================ */}
+                {this.userActionsLine()}
+                {this.duplicatesLine()}
+                {this.removeAndBanDialogs()}
+              </div>
+              <div class="col-4" id="expando-box">
+                {/* Post body prev or thumbnail */}
+                {!this.state.imageExpanded && this.thumbnail()}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    ) : (
+      <div class="d-block">
+        <div class="row">
+          {/* TEXT POST   ================ */}
+          {!this.props.viewOnly && this.voteBar()}
+          <div className="col-12" id="text-post">
+            {this.postTitleLine()}
+            {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
+            {this.showMobilePreview()}
+            {/* post by   ================ */}
+            {this.createdLine()}
+            {this.commentsLine(true)}
+            {/* {this.commentsLine()}*/}
+            {this.userActionsLine()}
+            {this.duplicatesLine()}
+            {this.removeAndBanDialogs()}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  // ======== END MY LISTING
 
   voteBar() {
     return (

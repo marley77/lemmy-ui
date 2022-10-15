07b600bd60329bceb14e17f52bb0ba0233257b93
tk77 <tommyknight@protonmail.com>
2022-10-15 17:12:49 -0400

    Update post-listing.tsx

--- src/shared/components/post/post-listing.tsx
+++ src/shared/components/post/post-listing.tsx
@@ -237,38 +237,38 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
       <div class="d-block">
         <div class="row">
           {!this.props.viewOnly && this.voteBar()}
-          <div>
+          {/*          <div>
+            {this.postTitleLine()}*/}
+          <div
+            className={`${
+              this.state.imageExpanded
+                ? "col-12 disco-wide"
+                : "col-8 disco-thin"
+            }`}
+          >
             {this.postTitleLine()}
-            <div
-              className={`${
-                this.state.imageExpanded
-                  ? "col-12 disco-wide"
-                  : "col-8 disco-thin"
-              }`}
-            >
-              {/*            {this.postTitleLine()}*/}
-              {/* MOBILE THUMBNAIL ONLY */}
-              <div id="mobile-thumb">
-                {/* Post body prev or thumbnail */}
-                {!this.state.imageExpanded && this.thumbnail()}
-              </div>
-              {/*  THE BIG IMAGE moved here */}
-              {this.state.imageExpanded && this.img}
-              {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
-              {this.showMobilePreview()}
-              {/* post by   ================ */}
-              {this.createdLine()}
-              {this.commentsLine(true)}
-              {/*  {this.commentsLine()}   ================ */}
-              {this.userActionsLine()}
-              {this.duplicatesLine()}
-              {this.removeAndBanDialogs()}
-            </div>
-            <div class="col-4" id="pc-expando">
+            {/* MOBILE THUMBNAIL ONLY */}
+            <div id="mobile-thumb">
               {/* Post body prev or thumbnail */}
               {!this.state.imageExpanded && this.thumbnail()}
             </div>
+            {/*  THE BIG IMAGE moved here */}
+            {this.state.imageExpanded && this.img}
+            {/* SNIPPET + TEXT mobile - Show a preview of the post body */}
+            {this.showMobilePreview()}
+            {/* post by   ================ */}
+            {this.createdLine()}
+            {this.commentsLine(true)}
+            {/*  {this.commentsLine()}   ================ */}
+            {this.userActionsLine()}
+            {this.duplicatesLine()}
+            {this.removeAndBanDialogs()}
           </div>
+          <div class="col-4" id="pc-expando">
+            {/* Post body prev or thumbnail */}
+            {!this.state.imageExpanded && this.thumbnail()}
+          </div>
+          {/*</div>*/}
         </div>
       </div>
     ) : (

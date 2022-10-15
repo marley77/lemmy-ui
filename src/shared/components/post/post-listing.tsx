749d1c0c75a5312d10993d3875d73590dd58eabb
tk77 <tommyknight@protonmail.com>
2022-10-15 13:19:19 -0400

    thumb optimize, nav icon

--- src/shared/components/post/post-listing.tsx
+++ src/shared/components/post/post-listing.tsx
@@ -237,7 +237,7 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
       <div class="d-block">
         <div class="row">
           {!this.props.viewOnly && this.voteBar()}
-
+          {this.postTitleLine()}
           <div
             className={`${
               this.state.imageExpanded
@@ -245,7 +245,7 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
                 : "col-8 disco-thin"
             }`}
           >
-            {this.postTitleLine()}
+            {/*            {this.postTitleLine()}*/}
             {/* MOBILE THUMBNAIL ONLY */}
             <div id="mobile-thumb">
               {/* Post body prev or thumbnail */}

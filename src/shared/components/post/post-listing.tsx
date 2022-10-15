00467b539ecbed2de0ced8fea77c237ec087f6ec
tk77 <tommyknight@protonmail.com>
2022-10-15 13:31:28 -0400

    post listing title col

--- src/shared/components/post/post-listing.tsx
+++ src/shared/components/post/post-listing.tsx
@@ -237,7 +237,7 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
       <div class="d-block">
         <div class="row">
           {!this.props.viewOnly && this.voteBar()}
-          {this.postTitleLine()}
+          <div class="col-12">{this.postTitleLine()}</div>
           <div
             className={`${
               this.state.imageExpanded

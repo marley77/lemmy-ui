53b5aa4d50708c5a6dcad2c7d544c18e40e17760
tk <tommyknight@protonmail.com>
2023-03-10 14:14:33 -0500

    Update symbols.tsx

--- src/shared/components/common/symbols.tsx
+++ src/shared/components/common/symbols.tsx
@@ -19,25 +19,25 @@ export const SYMBOLS = (
       
 
 <div id="topastro" onclick="boostmenu()"><svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="gripfire" class="svg-inline--fa fa-gripfire fa-w-12" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M112.5 301.4c0-73.8 105.1-122.5 105.1-203 0-47.1-34-88-39.1-90.4.4 3.3.6 6.7.6 10C179.1 110.1 32 171.9 32 286.6c0 49.8 32.2 79.2 66.5 108.3 65.1 46.7 78.1 71.4 78.1 86.6 0 10.1-4.8 17-4.8 22.3 13.1-16.7 17.4-31.9 17.5-46.4 0-29.6-21.7-56.3-44.2-86.5-16-22.3-32.6-42.6-32.6-69.5zm205.3-39c-12.1-66.8-78-124.4-94.7-130.9l4 7.2c2.4 5.1 3.4 10.9 3.4 17.1 0 44.7-54.2 111.2-56.6 116.7-2.2 5.1-3.2 10.5-3.2 15.8 0 20.1 15.2 42.1 17.9 42.1 2.4 0 56.6-55.4 58.1-87.7 6.4 11.7 9.1 22.6 9.1 33.4 0 41.2-41.8 96.9-41.8 96.9 0 11.6 31.9 53.2 35.5 53.2 1 0 2.2-1.4 3.2-2.4 37.9-39.3 67.3-85 67.3-136.8 0-8-.7-16.2-2.2-24.6z"></path></svg> BIG LIFE BOOST <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sort" class="svg-sort" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path></svg>
-</div><br>
+</div><br/>
 
 <div id="bm-wrap" style="display: none;">
 
   <div id="tito"> 
 
-        <br><a href="//biglifeboost.quest/enlightenment/">✧ Enlightenment</a><br>
-        <a href="//biglifeboost.quest/champion/">✧ Champion</a><br>
-        <a href="//biglifeboost.quest/relationships/">✧ Relationships</a><br>
-        <a href="//biglifeboost.quest/wisdom/">✧ Wiz</a><br>
+        <br/><a href="//biglifeboost.quest/enlightenment/">✧ Enlightenment</a><br/>
+        <a href="//biglifeboost.quest/champion/">✧ Champion</a><br/>
+        <a href="//biglifeboost.quest/relationships/">✧ Relationships</a><br/>
+        <a href="//biglifeboost.quest/wisdom/">✧ Wiz</a><br/>
         <a href="//biglifeboost.quest/">✧ Community</a> 
 
 
     <div id="mobile-nav-block">
       <p>
-        All the way<br>
-        Total devotion<br>
-        Bravery and strength<br>
-        Honor and leadership<br>
+        All the way<br/>
+        Total devotion<br/>
+        Bravery and strength<br/>
+        Honor and leadership<br/>
       </p>
     </div>
 
@@ -64,7 +64,7 @@ export const SYMBOLS = (
 
   <div id="tito">
 
-  <br><a href="//biglifeboost.quest/tags/enlightenment/"><span id="boosticon"><i class="fab fa-gripfire"></i></span>ENLIGHTENMENT &gt;</a><br>
+  <br/><a href="//biglifeboost.quest/tags/enlightenment/"><span id="boosticon"><i class="fab fa-gripfire"></i></span>ENLIGHTENMENT &gt;</a><br/>
   <div id="linkies">
   <ul><li><a href="https://biglifeboost.quest/post/tao-te-ching/">Widsom of The Tao Te Ching - Lao Tzu &gt;</a></li>
   <li><a href="https://biglifeboost.quest/post/20-minute-meditation/">20-Minute Morning Meditation...Amazing &gt;</a></li>
@@ -72,23 +72,23 @@ export const SYMBOLS = (
   </ul>
   </div>
 
-  <a href="//biglifeboost.quest/tags/evolution/"><span id="boosticon"><i class="fas fa-mountain"></i></span>EVOLUTION &gt;</a><br>
+  <a href="//biglifeboost.quest/tags/evolution/"><span id="boosticon"><i class="fas fa-mountain"></i></span>EVOLUTION &gt;</a><br/>
   <div id="linkies">
   <ul><li><a href="https://biglifeboost.quest/post/tai-chi/">8-Minute Tai-Chi &gt;</a></li>
   <li><a href="https://biglifeboost.quest/post/cold-shower-superpower/">The Cold Shower Superpower &gt;</a></li>
   <li><a href="https://biglifeboost.quest/post/yoga/">9-Minute Yoga – Good poses for beginners &gt;</a></li></ul>
   </div>
 
-  <a href="//biglifeboost.quest/tags/relationships/"><span id="boosticon"><i class="fas fa-user-friends"></i></span>RELATIONSHIPS &gt;</a><br>
+  <a href="//biglifeboost.quest/tags/relationships/"><span id="boosticon"><i class="fas fa-user-friends"></i></span>RELATIONSHIPS &gt;</a><br/>
   <div id="linkies">
   <ul><li><a href="https://biglifeboost.quest/post/break-up/">How to Get Over a Break-Up &gt;</a></li>
   <li><a href="https://biglifeboost.quest/post/new-friend/">How to Make a New Best Friend &gt;</a></li></ul>
-  </div><br>
+  </div><br/>
 
   <div id="bothalf">
-    <a href="//biglifeboost.quest/videos/"><span id="boosticon"><i class="fas fa-video"></i></span>VIDEOS &gt;</a><br>
-    <a href="https://vibe.biglifeboost.com"><span id="boosticon"><i class="far fa-comments"></i></span>COMMUNITY &gt;</a><br>
-    <a href="//biglifeboost.quest/all-boosts/"><span id="boosticon">✧</span>ALL BOOSTS &gt;</a><br><span id="boosticon"> </span>
+    <a href="//biglifeboost.quest/videos/"><span id="boosticon"><i class="fas fa-video"></i></span>VIDEOS &gt;</a><br/>
+    <a href="https://vibe.biglifeboost.com"><span id="boosticon"><i class="far fa-comments"></i></span>COMMUNITY &gt;</a><br/>
+    <a href="//biglifeboost.quest/all-boosts/"><span id="boosticon">✧</span>ALL BOOSTS &gt;</a><br/><span id="boosticon"> </span>
   </div>
   
   
